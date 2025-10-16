01b88162b430e563fa26ad24b0728061d3469993
epiprokash <jucse28.370@gmail.com>
2025-10-16 18:05:33 +0600

    Fixed the issues

--- src/main/java/com/example/StudentRbacApplication/security/JwtFilter.java
+++ src/main/java/com/example/StudentRbacApplication/security/JwtFilter.java
@@ -1,18 +1,19 @@
 package com.example.StudentRbacApplication.security;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import com.auth0.jwt.interfaces.DecodedJWT;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.context.SimpleGrantedAuthority;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-import java.util.List;
 
 public class JwtFilter extends OncePerRequestFilter {
 
